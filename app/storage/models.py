50f794f35a0cfb09ae32e79fe8c5d34442ed509a
Razvan Chitu <razvan3895@yahoo.com>
2015-03-29 18:05:42 +0300

    Define MOAAAAAAAAAAR

--- app/storage/models.py
+++ app/storage/models.py
@@ -5,3 +5,18 @@ class Deck(object):
     def __init__(self, name):
         self.name = name
 
+
+class Card(object):
+
+	def __init__(self, deck, content, type, answers):
+		self.deck = deck
+		self.type = type
+		self.content = content
+		self.answers = answers
+
+
+class Answer(object):
+
+	def __init__(self, card, a_text):
+		self.a_text
+		self.card = card
